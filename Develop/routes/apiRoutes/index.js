48ff7482cb8813f6c880ba5800dd297a8a2680c5
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-05-19 11:59:14 -0400

    logging

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,21 +1,23 @@
 const router = require('express').Router();
-// const { notes } = require('../notes');
+const { notes } = require('../notes');
 
-//get * index html file//
-router.get('/index', (req, res) => {
-    // res.json(index);
-});
-
-//get/notes html file//
-    router.get('/notes', (req, res) => {
-        // res.json(notes);
-    });
 
 //get api/notes from db.json//
+router.get('/notes', (req, res) => {
 
+});
 
 
 
 //post api/notes added to db.json and returned to client 
+router.post('/notes', (req, res) => { console.log (req.body);
+// req.body.title = notes.length.toString();
+
+// if(req.body ===null) {
+//     res.status(400).send('Please fill out Title for note');
+// } else {
+//     res.
+// }
+});
 
 module.exports = router;
