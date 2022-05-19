99254c86a79c758a6d3bbe53e052de1c175f62d7
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-05-19 11:25:03 -0400

    commenting out require

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { notes } = require('../notes');
+// const { notes } = require('../notes');
 
 //get * index html file//
 router.get('/index', (req, res) => {
